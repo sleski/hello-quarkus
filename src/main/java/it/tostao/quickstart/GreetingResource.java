1f624a931286f3af5835fdbc6b85fa1cb60f60f8
Slawomir Leski <slawomir.leski@gmail.com>
2022-03-08 21:39:23 +0100

    prepared demo

--- src/main/java/it/tostao/quickstart/GreetingResource.java
+++ src/main/java/it/tostao/quickstart/GreetingResource.java
@@ -4,14 +4,26 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.LocalDateTime;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello My Friend! " + LocalDateTime.now();
-    }
+	private final static String OLD_MAN_SMILING_PNG = "/images/old-man-smiling.png";
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		try {
+			URL url = GreetingResource.class.getResource(OLD_MAN_SMILING_PNG);
+			File file = new File(url.toURI());
+			return "Hello My Friend! File size in bytes = " + file.length();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return "Hello My Friend! " + LocalDateTime.now() + ", ERROR!";
+		}
+	}
 }
